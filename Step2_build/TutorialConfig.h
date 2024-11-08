7a9842b297dd870191454576388abbbea3ad82d8
Ky Anh <anh261104@gmail.com>
2024-11-08 13:57:36 +0700

    add Step2_build directory

---
+++ Step2_build/TutorialConfig.h
@@ -0,0 +1,6 @@
+// the configured options and settings for Tutorial
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
+
+#define PROJECT_BINARY_DIR /home/kyanh/Desktop/LinuxNetwork/cmake_prj_template/cmake-3.31.0-rc2-tutorial-source/Step2_build
+#define PROJECT_SOURCE_DIR /home/kyanh/Desktop/LinuxNetwork/cmake_prj_template/cmake-3.31.0-rc2-tutorial-source/Step2
